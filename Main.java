e2c1e7e71db59c6bcdf030194625435525261f0a
SmallPlayz <96578565+SmallPlayz@users.noreply.github.com>
2022-10-14 08:23:21 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,84 @@
+class Main extends ConsoleColors{
+  public static void main(String[] args) {
+    
+    System.out.println(RESET + "Reset");
+
+    System.out.println();
+    
+    System.out.println(RED + "RED");
+    System.out.println(GREEN + "GREEN");
+    System.out.println(YELLOW + "YELLOW");
+    System.out.println(BLUE + "BLUE");
+    System.out.println(PURPLE + "PURPLE");
+    System.out.println(CYAN + "CYAN");
+    System.out.println(WHITE + "WHITE");
+
+    System.out.println();
+    
+    System.out.println(BLACK_BOLD + "BLACK_BOLD");
+    System.out.println(RED_BOLD + "RED_BOLD");
+    System.out.println(GREEN_BOLD + "GREEN_BOLD");
+    System.out.println(YELLOW_BOLD + "YELLOW_BOLD");
+    System.out.println(BLUE_BOLD + "BLUE_BOLD");
+    System.out.println(PURPLE_BOLD + "PURPLE_BOLD");
+    System.out.println(CYAN_BOLD + "CYAN_BOLD");
+    System.out.println(WHITE_BOLD + "WHITE_BOLD");
+
+    System.out.println();
+    
+    System.out.println(BLACK_UNDERLINED + "BLACK_UNDERLINED");
+    System.out.println(RED_UNDERLINED + "RED_UNDERLINED");
+    System.out.println(GREEN_UNDERLINED + "GREEN_UNDERLINED");
+    System.out.println(YELLOW_UNDERLINED + "YELLOW_UNDERLINED");
+    System.out.println(BLUE_UNDERLINED + "BLUE_UNDERLINED");
+    System.out.println(PURPLE_UNDERLINED + "PURPLE_UNDERLINED");
+    System.out.println(CYAN_UNDERLINED + "CYAN_UNDERLINED");
+    System.out.println(WHITE_UNDERLINED + "WHITE_UNDERLINED");
+
+    System.out.println();
+    
+    System.out.println(BLACK_BACKGROUND + "BLACK_BACKGROUND");
+    System.out.println(RED_BACKGROUND + "RED_BACKGROUND");
+    System.out.println(GREEN_BACKGROUND + "GREEN_BACKGROUND");
+    System.out.println(YELLOW_BACKGROUND + "YELLOW_BACKGROUND");
+    System.out.println(BLUE_BACKGROUND + "BLUE_BACKGROUND");
+    System.out.println(PURPLE_BACKGROUND + "PURPLE_BACKGROUND");
+    System.out.println(RESET);
+    System.out.println(CYAN_BACKGROUND + "CYAN_BACKGROUND");
+    System.out.println(WHITE_BACKGROUND + "WHITE_BACKGROUND");
+
+    System.out.println();
+
+    System.out.println(BLACK_BRIGHT + "BLACK_BRIGHT");
+    System.out.println(RED_BRIGHT + "RED_BRIGHT");
+    System.out.println(GREEN_BRIGHT + "GREEN_BRIGHT");
+    System.out.println(YELLOW_BRIGHT + "YELLOW_BRIGHT");
+    System.out.println(BLUE_BRIGHT + "BLUE_BRIGHT");
+    System.out.println(PURPLE_BRIGHT + "PURPLE_BRIGHT");
+    System.out.println(CYAN_BRIGHT + "CYAN_BRIGHT");
+    System.out.println(WHITE_BRIGHT + "WHITE_BRIGHT");
+
+    System.out.println();
+    
+    System.out.println(BLACK_BOLD_BRIGHT + "BLACK_BOLD_BRIGHT");
+    System.out.println(RED_BOLD_BRIGHT + "RED_BOLD_BRIGHT");
+    System.out.println(GREEN_BOLD_BRIGHT + "GREEN_BOLD_BRIGHT");
+    System.out.println(YELLOW_BOLD_BRIGHT + "YELLOW_BOLD_BRIGHT");
+    System.out.println(BLUE_BOLD_BRIGHT + "BLUE_BOLD_BRIGHT");
+    System.out.println(PURPLE_BOLD_BRIGHT + "PURPLE_BOLD_BRIGHT");
+    System.out.println(CYAN_BOLD_BRIGHT + "CYAN_BOLD_BRIGHT");
+    System.out.println(WHITE_BOLD_BRIGHT + "WHITE_BOLD_BRIGHT");
+
+    System.out.println();
+  
+    System.out.println(BLACK_BACKGROUND_BRIGHT + "BLACK_BACKGROUND_BRIGHT");
+    System.out.println(RED_BACKGROUND_BRIGHT + "RED_BACKGROUND_BRIGHT");    
+    System.out.println(GREEN_BACKGROUND_BRIGHT + "GREEN_BACKGROUND_BRIGHT");
+    System.out.println(YELLOW_BACKGROUND_BRIGHT + "YELLOW_BACKGROUND_BRIGHT");
+    System.out.println(BLUE_BACKGROUND_BRIGHT + "BLUE_BACKGROUND_BRIGHT");
+    System.out.println(PURPLE_BACKGROUND_BRIGHT + "PURPLE_BACKGROUND_BRIGHT");
+    System.out.println(CYAN_BACKGROUND_BRIGHT + "CYAN_BACKGROUND_BRIGHT");
+    System.out.println(WHITE_BACKGROUND_BRIGHT + "WHITE_BACKGROUND_BRIGHT");
+    
+  }
+}
